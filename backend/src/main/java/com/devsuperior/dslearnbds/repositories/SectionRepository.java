b23941f43433cfa0a5f3bf7af6d3238f4d4cffab
luanalimadeveloper <luanalima.developer@gmail.com>
2022-06-03 14:46:02 -0300

    Repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/SectionRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.repositories;
+import com.devsuperior.dslearnbds.entities.Role;
+import com.devsuperior.dslearnbds.entities.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectionRepository extends JpaRepository<Section, Long> {
+    
+}
+
